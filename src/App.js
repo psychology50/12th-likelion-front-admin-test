3c871dcaa29b023762cd9ca2261f73a19dda6f13
KimSound424 <ykj1512@gmail.com>
2023-12-22 23:56:06 +0900

    [Feature] Routing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import Main from "./pages/Main";
+import Calendar from "./pages/Calendar";
 
 function App() {
   return (
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/calendar" element={<Calendar />} />
       </Routes>
   );
 }
